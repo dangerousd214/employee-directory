5bad4de1a98dafd85f789f5c97b2efa9aa7b132a
Darren Watson <v_tec@live.com>
2021-07-19 14:18:27 -0500

    added api for random users and got it to render data to console

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import Api from './componets/api'
 
 function App() {
+
+  useEffect(() => {
+    Api.getEmployee()
+    .then(({data}) => {
+      console.log(data.results);
+    })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav></nav>
+      <main></main>
     </div>
-  );
-}
+  )};
 
 export default App;
